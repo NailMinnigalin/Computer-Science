9a9b0fe102b004001acce371c4764d4322ee7e39
NailMinnigalin <DisappearedCat@yandex.ru>
2024-11-01 12:03:26 +0300

    Implement SingleNumber + Intersection

--- Computer Science/DataStructures/Applications/HashSetApplications.cs
+++ Computer Science/DataStructures/Applications/HashSetApplications.cs
@@ -1,4 +1,6 @@
-﻿namespace ComputerScience.DataStructures.Applications
+﻿using System.Collections.Generic;
+
+namespace ComputerScience.DataStructures.Applications
 {
     public class HashSetApplications
     {
@@ -7,6 +9,21 @@
             HashSet<int> set = new HashSet<int>(nums);
 
             return set.Count != nums.Length;
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (int num in nums)
+            {
+                result ^= num;
+            }
+            return result;
+        }
+
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            return nums1.Intersect(nums2).ToArray();
         }
     }
 }
