d9edd124dc0708764f226c653f106e23d65aac62
NailMinnigalin <DisappearedCat@yandex.ru>
2024-11-01 11:45:22 +0300

    Implement ContainsDuplicateClass

---
+++ Computer Science/DataStructures/Applications/ContainsDuplicate.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputerScience.DataStructures.Applications
+{
+    public static class ContainsDuplicateClass
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+
+            return set.Count != nums.Length;
+        }
+    }
+}
