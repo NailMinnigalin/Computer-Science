61866eeb355dedbba39930a6f2603db93e6f5d1f
NailMinnigalin <DisappearedCat@yandex.ru>
2024-11-02 20:31:33 +0300

    Implemet IsHappy

--- Computer Science/DataStructures/Applications/HashSetApplications.cs
+++ Computer Science/DataStructures/Applications/HashSetApplications.cs
@@ -25,5 +25,30 @@ namespace ComputerScience.DataStructures.Applications
         {
             return nums1.Intersect(nums2).ToArray();
         }
+
+        public bool IsHappy(int n)
+        {
+            HashSet<int> seenNumbers = new HashSet<int>();
+
+            while (n != 1 && !seenNumbers.Contains(n))
+            {
+                seenNumbers.Add(n);
+                n = GetSumOfSquares(n);
+            }
+
+            return n == 1;
+        }
+
+        private int GetSumOfSquares(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit * digit;
+                number /= 10;
+            }
+            return sum;
+        }
     }
 }
